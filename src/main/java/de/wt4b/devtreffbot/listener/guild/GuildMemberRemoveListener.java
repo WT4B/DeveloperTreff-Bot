d43396321ed097384130defeece67e25315420f3
WT4B <nxcoop19@gmail.com>
2021-05-11 23:48:05 +0200

    changed quit message

--- src/main/java/de/wt4b/devtreffbot/listener/guild/GuildMemberRemoveListener.java
+++ src/main/java/de/wt4b/devtreffbot/listener/guild/GuildMemberRemoveListener.java
@@ -1,6 +1,10 @@
 package de.wt4b.devtreffbot.listener.guild;
 
-import net.dv8tion.jda.api.entities.*;
+import de.wt4b.devtreffbot.manager.EmoteManager;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +16,8 @@ public class GuildMemberRemoveListener extends ListenerAdapter {
         Guild guild = event.getGuild();
         TextChannel textChannel = guild.getTextChannelById(840682766282391572L);
         if(textChannel == null) return;
-        textChannel.sendMessage(":red_circle: " + event.getUser().getAsMention() + " hat den Server verlassen.").queue();
+        EmoteManager emoteManager = EmoteManager.getInstance();
+        textChannel.sendMessage("[**-**] " + event.getUser().getAsMention() + " " + emoteManager.getEmoteAsMention(guild, "peepoBye")).queue();
         Member member = event.getMember();
         if(member == null) return;
         for(Role role : member.getRoles()) guild.removeRoleFromMember(member, role).queue();
